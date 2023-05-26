143f078ccdea23eb43540160b62ad6cc98f4c2b7
TaeSym <shauntesymone@gmail.com>
2023-05-26 00:30:45 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Write your JavaScript below this line
+var name = "Shaunte";
+var favoriteSnack = "sour candy";
+console.log(
+  "Hi, my name is " +
+    name +
+    " and I wish I had " +
+    favoriteSnack +
+    " right now."
+);
